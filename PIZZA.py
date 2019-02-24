f4f25d42677b4ca8cd39653076e23c7e41f42ab9
vboiko <dev.vboiko@gmail.com>
2019-02-24 13:40:51 +0200

    razpanah

--- PIZZA.py
+++ PIZZA.py
@@ -1,6 +1,6 @@
 from Task import Task
 
-def get_possible_shapes(task):
+def get_possible_shapes_v1(task):
     min_size = task.min_ing * 2
     shape = []
     i = 0
@@ -9,10 +9,27 @@ def get_possible_shapes(task):
         while (shape[-1][0] + 1) * (shape[-1][1] + 2) <= task.max_cells:
             shape.append([shape[-1][0], shape[-1][1] + 1])
 
-        if (shape[-1][0] + 2) * 1 > task.max_cells:
+        if (shape[-1][0] + 1) * (shape[-1][1] + 1)  > task.max_cells:
             break
-        shape.append([shape[-1][0] + 1, 0])
+        m = 0
+        while (shape[-1][0] + 2) * (m + 1) < min_size:
+            m += 1
+        shape.append([shape[-1][0] + 1, m])
     return shape
+
+
+def get_possible_shapes(task):
+    min_size = task.min_ing * 2
+    shape = []
+    for i in range(task.max_cells):
+        for j in range(task.max_cells):
+            shape.append([i, j])
+    out = []
+    for sh in shape:
+        if (sh[0] + 1) * (sh[1] + 1) >=min_size and (sh[0] + 1) * (sh[1] + 1) <= task.max_cells:
+            out.append(sh)
+    return out
+
 
 
 def read_file(fname):
@@ -25,7 +42,6 @@ def read_file(fname):
 
 def get_input(filename):
     task = read_file(filename)
-    task.max_cells = 4
     shapes = get_possible_shapes(task)
     return shapes, task
 
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from Answer import Answer
+from Answer import Answer, Piece
 from PIZZA import get_input
 import numpy as np
 
 ans = Answer()
 
-def check_valid(task, v, i, j, piece):
+def check_valid(task, v, i, j, piece, num=0):
     t = 0
     m = 0
     for k in range(piece[0] + 1):
         for l in range(piece[1] + 1):
-            if i + k >= task.rows or j + l >= task.columns:
+            if i + k >= task.rows or j + l >= task.columns or i + k < 0 or j + l < 0:
                 return False
-            if v[i + k][j + l] != 0:
+            if v[i + k][j + l] != 0 and v[i + k][j + l] != num:
                 return False
             if task.map[i + k][j + l] == 'T':
                 t += 1
@@ -26,6 +26,7 @@ def walk_pizza(task, pieces):
     global ans
     arr = task.map
     v = np.zeros((task.rows, task.columns))
+    pie_count = 1
     for i in range(task.rows):
         for j in range(task.columns):
             for piece in pieces:
@@ -33,7 +34,27 @@ def walk_pizza(task, pieces):
                     ans.put([i, j, i + piece[0], j + piece[1]])
                     for k in range(piece[0] + 1):
                         for l in range(piece[1] + 1):
-                            v[i + k][j + l] = 1
+                            v[i + k][j + l] = pie_count
+                    pie_count += 1
+    return v
+
+
+def razpanah(task, v, pieces):
+    global ans
+    for pie_coord in range(len(ans.all)):
+        pie = ans.all[pie_coord]
+        for big_pie in pieces[::-1]:
+            if (big_pie[0] + 1) * (big_pie[1] + 1) > (pie.xe + 1) * (pie.ye + 1):
+                pie_n = v[pie.xs][pie.ys]
+                st_poi = [pie.xs - big_pie[0], pie.ys - big_pie[1]]
+                for i in range(big_pie[0] + 1):
+                    for j in range(big_pie[1] + 1):
+                        if check_valid(task, v, st_poi[0] + i, st_poi[1] + j, big_pie, num=pie_n):
+                            v[v == pie_n] = 0
+                            ans.all[pie_coord] = Piece(st_poi[0] + i, st_poi[1] + j, st_poi[0] + i + big_pie[0], st_poi[1] + j + big_pie[1])
+                            for k in range(big_pie[0] + 1):
+                                for l in range(big_pie[1] + 1):
+                                    v[st_poi[0] + i + k][st_poi[1] + j + l] = pie_n
 
 
 def go_all():
@@ -49,7 +70,9 @@ def main(file):
     global ans
     a, task = get_input(file)
     a.sort(key=lambda x: (x[0] + 1) * (x[1] + 1))
-    walk_pizza(task, a)
+    v = walk_pizza(task, a)
+    razpanah(task, v, a)
     print("DONE", file)
 
 go_all()
+# main('c_medium.in')
